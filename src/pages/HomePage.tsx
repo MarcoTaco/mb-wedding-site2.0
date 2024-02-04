703b721a71b2aa5240c021e8a662150bcf905232
Marco <marco.a.romero0303@gmail.com>
2024-02-04 00:55:12 -0800

    styling the home page

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -8,65 +8,44 @@ function HomePage(){
                 <div className="home-intro-section">
                     <div className="home-intro">
                         <div className="home-text">
-                            <p>When?</p>
-                            <p>September 7th, 2024</p>
+                            <p className="big-text">When?</p>
+                            <p className="small-text">September 7th, 2024</p>
                         </div>
                         <div className="middle-section">
                             <div className="arched-text">
-                                <span className="character">T</span>
-                                <span className="character">H</span>
-                                <span className="character">E</span>
-                                <span className="character"></span>
-                                <span className="character">W</span>
-                                <span className="character">E</span>
-                                <span className="character">D</span>
-                                <span className="character">D</span>
-                                <span className="character">I</span>
-                                <span className="character">N</span>
-                                <span className="character">G</span>
-                                {/* <span className="character"></span>
-                                <span className="character">O</span>
-                                <span className="character">F</span>
-                                <span className="character"></span>
-                                <span className="character">M</span>
-                                <span className="character">A</span>
-                                <span className="character">R</span>
-                                <span className="character">C</span>
-                                <span className="character">O</span>
+                                <span className="character">THE</span>
+                                <span className="character">WEDDING</span>
+                                <span className="character">OF</span>
+                                <span className="character">MAR</span>
+                                <span className="character">CO</span>
                                 <span className="character">+</span>
-                                <span className="character">B</span>
-                                <span className="character">R</span>
-                                <span className="character">I</span>
-                                <span className="character">T</span>
-                                <span className="character">T</span>
-                                <span className="character">A</span>
-                                <span className="character">N</span>
-                                <span className="character">Y</span> */}
+                                <span className="character">BRIT</span>
+                                <span className="character">TANY</span>
                             </div>
                             <div className="middle-picture">
                                 <div className="img-holder"></div>
                             </div>
                         </div>
                         <div className="home-text">
-                            <p>Where:</p>
-                            <p>The Aurora Colony Vineyards.</p>
-                            <p>21338 Oak Ln NE, Aurora, OR 97002</p>
+                            <p className="big-text">Where:</p>
+                            <p className="small-text">The Aurora Colony Vineyards.</p>
+                            <p className="small-text">21338 Oak Ln NE, Aurora, OR 97002</p>
                         </div>
                     </div>
                 </div>
                 <div className="wedding-info">
                     <div className="ceremony-info">
-                        <p>Ceremony</p>
-                        <p>3:30pm</p>
-                        <p>Aurora Colony Vineyards</p>
+                        <p className="big-text">Ceremony</p>
+                        <p className="big-text">3:30pm</p>
+                        <p className="small-text">Aurora Colony Vineyards</p>
                     </div>
                     <div className="plus">
                         <p>+</p>
                     </div>
                     <div className="reception-info">
-                        <p>Reception</p>
-                        <p>4:30pm</p>
-                        <p>Aurora Colony Vineyards</p>
+                        <p className="big-text">Reception</p>
+                        <p className="big-text">4:30pm</p>
+                        <p className="small-text">Aurora Colony Vineyards</p>
                     </div>
                     <div className="rsvp-button">
                         <button>RSVP &gt;</button>
@@ -74,16 +53,18 @@ function HomePage(){
                 </div>
                 <div className="faq-section">
                     <div className="faqs">
-                        <p>Questions?</p>
-                        <p>Check out our FAQ page</p>
+                        <p className="big-text">Questions?</p>
+                        <p className="small-text">Check out our FAQ page</p>
                     </div>
                 </div>
                 <div className="registry-section">
-                    <p>Your presence is enough of a gift... But if you must</p>
-                    <p>Here's our Registry</p>
+                    <p className="big-text">Your presence is enough of a gift... But if you must</p>
+                    <p className="small-text">Here's our Registry</p>
                 </div>
-                <div className="home-final-message">
-                    <p>Join us in celebrating our love and creating memories that will last a lifetime!</p>
+                <div className="home-final-message-section">
+                    <div className="home-final-message">
+                        <p>Join us in celebrating our love and creating memories that will last a lifetime!</p>
+                    </div>
                 </div>
             </div>
         </div>
