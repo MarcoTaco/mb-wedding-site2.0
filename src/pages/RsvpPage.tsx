645384e295947c4788a6456a5da6af2e3ed84d10
Marco <marco.a.romero0303@gmail.com>
2024-02-18 09:54:18 -0800

    finishing rsvp section

--- src/pages/RsvpPage.tsx
+++ src/pages/RsvpPage.tsx
@@ -5,19 +5,23 @@ import "../styles/RsvpPage.css";
 function RsvpPage(){
     return(
         <div className="rsvp-main">
-            <div className="rsvp-text-section">
-                <div className="rsvp-header">
-                    <h3>See you soon?</h3>
-                </div>
-                <div className="rsvp-text">
-                    <h5>1. </h5><p>Let us know if you'll be there!</p>
-                </div>
-                <div className="rsvp-text">
-                    <h5>2. </h5><p>Let us know what you would like to drink!</p>
+            <div className="rsvp-left-section">
+                <div className="rsvp-text-section">
+                    <div className="rsvp-header">
+                        <h3>See you soon?</h3>
+                    </div>
+                    <div className="rsvp-text">
+                        <p>1. </p><p>Let us know if you'll be there!</p>
+                    </div>
+                    <div className="rsvp-text">
+                        <p>2. </p><p> Let us know what you would like to drink!</p>
+                    </div>
                 </div>
             </div>
-            <div className="rsvp-form-section">
-                <RsvpForm />
+            <div className="rsvp-right-section">
+                <div className="rsvp-form-section">
+                    <RsvpForm />
+                </div>
             </div>
         </div>
     )
